8b4434f20a6e8dd5d1d8c4e922cdd6e735a63313
William (Lindy) Lindstrom <llindstrom@lco.global>
2026-06-11 15:06:30 -0700

    add newly clarified timestamps

--- tom_alertstreams/admin.py
+++ tom_alertstreams/admin.py
@@ -5,6 +5,6 @@ from tom_alertstreams.models import Alert
 
 @admin.register(Alert)
 class AlertAdmin(admin.ModelAdmin):
-    list_display = ('stream_name', 'alert_id', 'timestamp', 'object_id', 'magnitude', 'flux')
+    list_display = ('stream_name', 'alert_id', 'published_time', 'observation_time', 'object_id', 'magnitude', 'flux')
     list_filter = ('stream_name',)
     search_fields = ('alert_id', 'object_id')
